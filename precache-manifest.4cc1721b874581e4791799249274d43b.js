cc8b81376df5b690ba845e5aa86bfde6a75c081f
Jungdo Lee <lee.jungdo@gmail.com>
2018-12-21 18:02:36 +0900

    Updates

---
+++ precache-manifest.4cc1721b874581e4791799249274d43b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4902b54605b90a83a91",
+    "url": "/demo/react-on-screen-test/static/js/runtime~main.a4902b54.js"
+  },
+  {
+    "revision": "5dbf859afa9fe8b7a897",
+    "url": "/demo/react-on-screen-test/static/js/main.5dbf859a.chunk.js"
+  },
+  {
+    "revision": "720bc02299512e5f2a1a",
+    "url": "/demo/react-on-screen-test/static/js/1.720bc022.chunk.js"
+  },
+  {
+    "revision": "5dbf859afa9fe8b7a897",
+    "url": "/demo/react-on-screen-test/static/css/main.b1247435.chunk.css"
+  },
+  {
+    "revision": "0c474cae0d0132a75f2ff54c707da6e6",
+    "url": "/demo/react-on-screen-test/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-on-screen-test/precache-manifest.4462bb689359b7d3b2df73dd5f8e2f4c.js"
+  "/demo/react-on-screen-test/precache-manifest.4cc1721b874581e4791799249274d43b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-on-screen-test/index.html", {
+workbox.routing.registerNavigationRoute("/demo/react-on-screen-test/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
