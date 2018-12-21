aab0b82a126989eb02c8fd803dcfdf07237ddd7a
Jungdo Lee <lee.jungdo@gmail.com>
2018-12-21 12:21:56 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TrackVisibility from 'react-on-screen';
+
+const ComponentToTrack = ({ isVisible }) => {
+  const style = {
+    background: isVisible ? 'red' : 'blue',
+    height: '70vh',
+    border: '3px solid black',
+    padding: '30px',
+    margin: '10px'
+  };
+
+  return <div style={style}>{isVisible ? 'Visible' : 'Not visible'}</div>;
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <TrackVisibility>
+          <ComponentToTrack />
+        </TrackVisibility>
+        <TrackVisibility offset={500}>
+          <ComponentToTrack />
+        </TrackVisibility>
+        <TrackVisibility offset={700}>
+          <ComponentToTrack />
+        </TrackVisibility>
       </div>
     );
   }
